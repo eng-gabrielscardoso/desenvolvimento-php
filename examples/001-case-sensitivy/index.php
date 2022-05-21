e1a7dc33933ee5ae18aed28bdc177e169af38907
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-05-21 19:15:36 -0300

    feat: case sensitivy lesson

---
+++ examples/001-case-sensitivy/index.php
@@ -0,0 +1,15 @@
+<?php
+
+  # It's a BAD practice
+  echo 'testing<br />';
+  ecHO 'testin<br />';
+  eCho 'testing<br />';
+
+  $name = 'Gabriel';
+
+  # The variable $nAme doesn't exist, so will not be shown in the output
+  echo $name;
+  echo '<br />';
+  echo $nAme;
+
+?>

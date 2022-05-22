7b9f51b5c0b8804e6461959889caba46ff3b39d9
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-05-21 22:55:58 -0300

    feat: string lesson

---
+++ examples/data-types/005-strings/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * The PHP default doesn't stripe the HTML tags
+ * This is the reason why we could use <br /> tag without a HTML file extension
+ */
+echo "Testing text";
+echo "<br />";
+
+?>

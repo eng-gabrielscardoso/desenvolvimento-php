014c5a28923f1546741a0aae6ebd346255040f4a
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-05-21 22:52:32 -0300

    feat: check float lesson

--- examples/data-types/004-floats/index.php
+++ examples/data-types/004-floats/index.php
@@ -5,5 +5,12 @@ $random = rand() * 0.5;
 echo $random;
 echo "<br />";
 echo gettype($random);
+echo "<br />";
+
+if (is_float($random) == 1) {
+  echo "É um float!";
+} else {
+  echo "Não é um float!";
+}
 
 ?>

bbd5f3a4e8e352363710787ae31c82631267cf91
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-05-21 22:10:26 -0300

    feat: comments lesson

---
+++ examples/004-comments/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// This line prints "Something" in the screen
+echo "Something";
+
+# This comment break line
+echo "<br />";
+
+/**
+ * Long comment
+ * Is just a comment
+ * The next line print "Something" in the screen again
+ */
+echo "Something";
+
+?>

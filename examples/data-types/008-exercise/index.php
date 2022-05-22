ac0c2bc25c0d01a85dc4ea31f8c15259e926492b
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-05-21 23:12:31 -0300

    feat: exercise 8
    The exercise consists in use a associative array to describe a person and check if he/she have more than 18 years

---
+++ examples/data-types/008-exercise/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$Gabriel = [
+  "name" => "Gabriel",
+  "age" => 20,
+  "sex" => "male"
+];
+
+if ($Gabriel['age'] >= 18 ) {
+  echo "Pode entrar";
+} else {
+  echo "Não pode entrar";
+}
+
+?>

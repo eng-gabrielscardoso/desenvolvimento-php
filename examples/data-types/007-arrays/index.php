f8088e17a552f2cc38008ad42709a8f626f242ca
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-05-21 23:09:36 -0300

    feat: associative array lesson

--- examples/data-types/007-arrays/index.php
+++ examples/data-types/007-arrays/index.php
@@ -4,4 +4,13 @@ $names = ["Gabriel", "Fabiana", "Analua"];
 
 print_r($names);
 
+$employee = [
+  "name" => "Gabriel Santos Cardoso",
+  "occupation" => "Web Developer",
+  "hired" => "26-12-2015"
+];
+
+echo "<br />";
+echo $employee["occupation"];
+
 ?>

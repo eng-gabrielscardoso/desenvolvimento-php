834a58056e4e4725e4b631f96c14ce17eb03f486
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-05-21 22:13:43 -0300

    feat: second exercise
    The exercise consists in describe attributes that a person can have using abstraction and PHP

---
+++ examples/005-exercises/index.php
@@ -0,0 +1,15 @@
+<?php
+
+# This file contains a list of attributes that a person can have
+$name = "Gabriel";
+$age = 20;
+$sex = "male";
+$gender = "male";
+$birthday = "02/12/2001";
+$country = "Brazil";
+$homecity = "Barcarena";
+$occupation = "Web Developer";
+$university = "Federal University of Pará";
+$course = "Information Systems";
+
+?>
